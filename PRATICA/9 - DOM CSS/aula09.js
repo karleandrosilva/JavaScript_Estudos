9f61c3880af11ee4bd92f58e66e3b84fd6b7dac5
karleandrosilva <contatoparakarle@gmail.com>
2024-12-22 10:53:37 -0300

    update aula 08

---
+++ PRATICA/9 - DOM CSS/aula09.js
@@ -0,0 +1,6 @@
+let titulo = document.querySelector('h1')
+titulo.textContent = 'Aula 09 - Manipular CSS'
+
+let image = document.querySelector('#foto');
+image.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png') // Alteraçãi e mundança de propriedade com CSS
+image.setAttribute('width', '150px')
