54d812bc4b62e75cb5e81319d4366c473c918156
karleandrosilva <contatoparakarle@gmail.com>
2024-12-22 15:39:40 -0300

    update aula 09

--- PRATICA/9 - DOM CSS/aula09.js
+++ PRATICA/9 - DOM CSS/aula09.js
@@ -1,6 +1,35 @@
 let titulo = document.querySelector('h1')
 titulo.textContent = 'Aula 09 - Manipular CSS'
 
+//  Alteração e mundança de propriedade com CSS
 let image = document.querySelector('#foto');
-image.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png') // Alteraçãi e mundança de propriedade com CSS
-image.setAttribute('width', '150px')
+image.setAttribute('src', 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png') // 
+image.setAttribute('width', '150px');
+
+// Manipular CSS
+titulo.style.color = '#F0DB4F';
+titulo.style.backgroundColor = 'black';
+titulo.style.width = '300px';
+
+
+let tela = document.querySelector('main');
+
+// seleciona os elementos que tem os botoes
+let botaoDark = document.querySelector('#btdark');
+let botaoLight = document.querySelector('#btlight');
+
+// Quando tiver um evento de click nos botoes ele chama a funcao e executa
+botaoDark.addEventListener('click', modoDark); // addEventListener -> escutador de eventos
+botaoLight.addEventListener('click', modoLight);
+
+function modoDark() {
+    console.log("Modo Dark ativado");
+    tela.classList.add("dark");
+    tela.classList.remove("light");
+}
+
+function modoLight() {
+    console.log("Modo Light ativado");
+    tela.classList.add("light");
+    tela.classList.remove("dark");
+}
