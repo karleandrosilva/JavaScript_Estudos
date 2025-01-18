fe8a4eeaac374d18bd46d7a8df685fd25f99da5a
karleandrosilva <contatoparakarle@gmail.com>
2025-01-18 16:30:12 -0300

    update

---
+++ PRATICA/15 - DESAFIO/desafio.js
@@ -0,0 +1,11 @@
+// SELECIONA TODOS OS ELEMENTOS QUE DESEJA MANIPULAR
+
+let formulario = document.querySelector('form'); // pegando o id deles
+let nota1 = document.querySelector('nota1');
+let nota2 = document.querySelector('nota2');
+
+let cxMedia = document.querySelector('#resultadoMedia');
+let aviso = document.querySelector('#aviso');
+
+let btnEnviar = document.querySelector('#btnEnviar');
+let btnEnviar = document.querySelector('#btnEnviar');
