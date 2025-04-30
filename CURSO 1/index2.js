504a73fe5707446e36282d5e6cdae130e84d8bc7
karleandrosilva <contatoparakarle@gmail.com>
2025-04-29 22:42:08 -0300

    update aula 02

---
+++ CURSO 1/index2.js
@@ -0,0 +1,17 @@
+// variaveis: const - let - var
+// var = variavel global
+
+// const = variavel constante, não pode ser alterada
+const nome1 = "Karle";
+console.log(nome1);
+
+// let = variavel local
+let nome = "Karle";
+console.log(nome);
+nome = "Karleandro";
+console.log(nome);
+
+// recomendado utilizar const e let
+
+// boas praticas de variaveis
+const meuNome = "Karleandro"; // camelCase
