42080952bf07e3ec7782644f70e9979d9ae8b8d9
karleandrosilva <contatoparakarle@gmail.com>
2025-05-02 00:26:10 -0300

    update aula 03

---
+++ CURSO 1/index3.js
@@ -0,0 +1,21 @@
+// operadores aritmeticos
+
+const sum = 10 + 5 // soma
+const sub = 10 - 5 // subtração
+const mult = 10 * 5 // multiplicação
+const div = 10 / 5 // divisão
+const rest = 10 % 5 // módulo - resto da divisão
+
+console.log("Soma: ", sum)
+console.log("Subtração: ", sub)
+console.log("Multiplicação: ", mult)
+console.log("Divisão: ", div)
+console.log("Resto da divisão: ", rest)
+
+const x = 6
+const y = 4
+const result = x + y
+
+console.log(result) // imprime o resultado da soma de x e y
+
+console.log((x + y) * 2) // imprime o resultado da soma de x e y multiplicado por 2 
