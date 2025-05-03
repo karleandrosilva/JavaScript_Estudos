c2bf5e6e75c914973626ee786c302c4811e0ab71
karleandrosilva <contatoparakarle@gmail.com>
2025-05-02 23:53:23 -0300

    update ex 03.2

---
+++ CURSO 1/index3.3.js
@@ -0,0 +1,12 @@
+const minute = 60;
+const hour = 60 * minute;
+const day = 24 * hour;
+const week = 7 * day;
+const year = 365 * day;
+
+console.log(`Um minuto tem ${minute} segundos`);
+console.log(`Uma hora tem ${hour} segundos`);
+console.log(`Um dia tem ${day} segundos`);
+console.log(`Uma semana tem ${week} segundos`);
+console.log(`Um ano tem ${year} segundos`);
+
