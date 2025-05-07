8d2df4a11910e4470b2c1a3b2f3e2f0ffb628cc7
karleandrosilva <contatoparakarle@gmail.com>
2025-05-06 23:28:10 -0300

    update aula 6

---
+++ CURSO 1/index6.js
@@ -0,0 +1,44 @@
+// comparacação
+
+// retornar verdadeiro ou falso
+
+// maior que: >
+console.log(10 > 5) // true
+console.log(10 > 12) // false
+
+// menor que: <
+console.log(10 < 5) // false
+console.log(10 < 12) // true
+
+// maior ou igual a: >=
+console.log(10 >= 5) // true
+console.log(10 >= 10) // true
+
+// menor ou igual a: <=
+console.log(10 <= 5) // false
+console.log(10 <= 10) // true
+
+// comparação ou igual a: == 
+console.log(10 == 5) // false
+console.log(10 == 10) // true
+
+// diferença ou diferente de: !=
+console.log(10 != 5) // true
+console.log(10 != 10) // false
+
+// estritamente igual a: === (compara o tipo e o valor)
+console.log(10 === 10) // true
+console.log(10 === "10") // false, pois o tipo é diferente (number e string)
+console.log("10" === "10") // true, pois o tipo é igual (string e string)
+
+// estritamente diferente de: !== (compara o tipo e o valor)
+console.log(10 !== 10) // false, pois o tipo e o valor são iguais (number e number)
+console.log(10 !== "10") // true, pois o tipo é diferente (number e string)
+console.log("10" !== "10") // false, pois o tipo e o valor são iguais (string e string)
+
+
+// testes
+console.log("Karle" == "Karle") // true, pois o valor é igual (string e string)
+console.log("Karle" == "karle") // false, pois o valor é diferente, o k é minusculo
+
+console.log("Karle" != "Karleandro") // true, pois o valor é diferente 
