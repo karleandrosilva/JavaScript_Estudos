30f55ad50167ac00b1faaac815d8b9b7282a273b
karleandrosilva <contatoparakarle@gmail.com>
2025-06-12 23:53:37 -0300

    update aula 9 parte 1

---
+++ CURSO 1/index9.1.js
@@ -0,0 +1,24 @@
+// controle de fluxos
+
+// Estrutura condicional if e else
+// onde for verdadeiro, executa o bloco de código
+
+let temperatura = 25;
+
+// if = se
+if (temperatura >= 30) {
+    console.log("Está muito quente lá fora!");
+} else { // else = se não
+    console.log("A Temperatura está agradável lá fora!");
+}
+
+
+let grade= 7.5;
+
+if (grade >= 9) {
+    console.log("Aprovado!");
+} else if (grade >= 7) { // else if = se não se
+    console.log("Aprovado na média!");
+}else {
+    console.log("Reprovado!");
+}
