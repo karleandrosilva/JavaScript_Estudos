e846053802486a054fb13de64e5a0b608df89d8a
karleandrosilva <contatoparakarle@gmail.com>
2025-06-19 23:05:18 -0300

    update parte 2

---
+++ CURSO 1/index11.2.js
@@ -0,0 +1,6 @@
+const fruits = ["maça", "banana", "laranja", "uva", "abacaxi"];
+
+// printa todos os elementos do array
+for (let i = 0; i < fruits.length; i++) { // length = tamanho do array
+    console.log(fruits[i]);
+}
