bad57e5b689c6a1fac702c85946104f7ccf6a841
karleandrosilva <contatoparakarle@gmail.com>
2025-09-07 22:14:07 -0300

    update aula 21 parte 1

---
+++ CURSO 1/index21.1.js
@@ -0,0 +1,11 @@
+// CAMINHOS
+
+// para poder importar as funções de soma e subtração, terá que passar o caminho relativo até ela.
+
+// . e ./ = esta acessando o mesmo diretório do arquivo
+
+import { sub } from "./functions/math/sub.js"
+import { sum } from "./functions/math/sum.js"
+
+console.log(sub(10, 5));
+console.log(sum(10,5));
