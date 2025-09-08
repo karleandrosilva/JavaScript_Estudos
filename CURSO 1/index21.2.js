f5eed98bdd66dba8db525e4035bb0128de99c71f
karleandrosilva <contatoparakarle@gmail.com>
2025-09-07 22:34:57 -0300

    update parte 3

--- CURSO 1/index21.2.js
+++
@@ -1,7 +0,0 @@
-// CAMINHOS - PARTE 2
-
-import { sum } from "./functions/math/sum.js"
-import { fixedSum } from "./functions/utils/fixed.js"
-
-console.log(sum(6.5555,4.8885));
-console.log(fixedSum(6.5555,4.8885));
---
+++ CURSO 1/index21.3.js
@@ -0,0 +1,5 @@
+// CAMINHO - PARTE 3
+
+import { exec } from "./commands/exec.js"
+
+exec("Karle")
